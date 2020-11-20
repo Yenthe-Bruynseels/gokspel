93174c129109d7906e385739e8a3ac649439647e
Lennert Van Oosterwyck <lennert.vanoosterwyck13@gmail.com>
2020-11-20 17:17:08 +0100

    s

--- src/model/database/SpelersDatabaseInMemory.java
+++ src/model/database/SpelersDatabaseInMemory.java
@@ -11,6 +11,7 @@ import java.util.*;
 
 
 public class SpelersDatabaseInMemory {
+
     // hashmap met als key de spelernaam want die moet uniek zijn en als value de speler
     Map<String, Speler> spelers = new HashMap<>();
 

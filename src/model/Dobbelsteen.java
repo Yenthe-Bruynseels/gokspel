866b380f5a969595b655045d7d0427bf8b487849
Lennert Van Oosterwyck <lennert.vanoosterwyck13@gmail.com>
2020-12-15 20:55:45 +0100

    dobbelsteen toegevoegt

---
+++ src/model/Dobbelsteen.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Dobbelsteen {
+
+
+    public Dobbelsteen() {
+    }
+
+
+    public int werpDobbelsteen(){
+        return (int) (Math.random() * (6 + 1));
+    }
+
+}
